44bbaed0510b12e802b088f1870cc211f293c29c
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-11-03 22:04:52 +0700

    feat: metadata

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { health } from "./routes/health.js";
 import { paymentMiddleware } from "./middleware/payment.js";
 import { price } from "./routes/price.js";
@@ -11,6 +12,11 @@ export function init() {
     if (!PAYOUT_ADDRESS) throw new Error('PAYOUT_ADDRESS not set')
 
     const app = new Hono();
+
+    // static
+    app.use("/favicon.ico", serveStatic({ path: "./public/favicon.ico" }));
+    app.use("/og.png", serveStatic({ path: "./public/og.png" }));
+    app.use("/", serveStatic({ path: "./public/index.html" }));
 
     // public
     app.route("/health", health);
